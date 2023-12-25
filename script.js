a9af06688586c4c58e83f3985a0cc7cefcb81022
AnshulKahar2729 <anshulkahar2211@gmail.com>
2023-12-25 09:50:03 +0530

    1st Commit

---
+++ script.js
@@ -0,0 +1,171 @@
+const apikey = "dd3885dcf2e92fd802ae952d1223d2d4";
+const apiEndPath = "https://api.themoviedb.org/3";
+const imgPath = "https://image.tmdb.org/t/p/original";
+
+const apiPaths = {
+    fetchAllCategories : `${apiEndPath}/genre/movie/list?api_key=${apikey}`,
+    fetchMoviesList : (id) => `${apiEndPath}/discover/movie?api_key=${apikey}&with_genres=${id}`,
+    fetchTrending : `${apiEndPath}/trending/all/day?api_key=${apikey}&language=en-US`,
+    searchOnYoutube: (query) => `https://www.googleapis.com/youtube/v3/search?part=snippet&q=${query}&key=AIzaSyC0SZJkHFX-fQ7NrsxdI4l4mGwYuY4l7P8`
+}
+
+
+const btn = document.querySelector("button");
+
+
+function init() {
+    fetchAndBuildAllSections();
+    fetchTrendingMovies();
+}
+
+
+// function fetchAndbuildBannerSection() {
+//     const bannerCont = document.getElementById('banner-section');
+//     bannerCont.style.backgroundImage = `url('${imgPath}${movie.backdrop_path}')`;
+//     const div = document.getElementById("banner-content");
+//     const banner__title = document.getElementById("banner__title");
+//     banner__title.innerHTML = `${movie.title}`;
+//     const banner__info = document.getElementById("banner__info");
+//     banner__info.innerHTML = `${movie.overview && movie.overview.length > 200 ? movie.overview.slice(0,200).trim()+ '...':movie.overview}`;
+//     const banner__overview = document.getElementById("banner__overview");
+//     banner__overview.innerHTML = `<button class="action-button"><svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg" class="Hawkins-Icon Hawkins-Icon-Standard"><path d="M4 2.69127C4 1.93067 4.81547 1.44851 5.48192 1.81506L22.4069 11.1238C23.0977 11.5037 23.0977 12.4963 22.4069 12.8762L5.48192 22.1849C4.81546 22.5515 4 22.0693 4 21.3087V2.69127Z" fill="currentColor"></path></svg> &nbsp;&nbsp; Play</button>
+//     <button class="action-button"><svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg" class="Hawkins-Icon Hawkins-Icon-Standard"><path fill-rule="evenodd" clip-rule="evenodd" d="M12 3C7.02944 3 3 7.02944 3 12C3 16.9706 7.02944 21 12 21C16.9706 21 21 16.9706 21 12C21 7.02944 16.9706 3 12 3ZM1 12C1 5.92487 5.92487 1 12 1C18.0751 1 23 5.92487 23 12C23 18.0751 18.0751 23 12 23C5.92487 23 1 18.0751 1 12ZM13 10V18H11V10H13ZM12 8.5C12.8284 8.5 13.5 7.82843 13.5 7C13.5 6.17157 12.8284 5.5 12 5.5C11.1716 5.5 10.5 6.17157 10.5 7C10.5 7.82843 11.1716 8.5 12 8.5Z" fill="currentColor"></path></svg> &nbsp;&nbsp; More Info</button>`;
+
+//     // div.className = "banner-content container";
+//     const buttonDiv = document.getElementById("action-button-cont");
+//     buttonDiv.innerHTML = `
+//     <button class="action-button b1"><svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg" class="Hawkins-Icon Hawkins-Icon-Standard"><path d="M4 2.69127C4 1.93067 4.81547 1.44851 5.48192 1.81506L22.4069 11.1238C23.0977 11.5037 23.0977 12.4963 22.4069 12.8762L5.48192 22.1849C4.81546 22.5515 4 22.0693 4 21.3087V2.69127Z" fill="currentColor"></path></svg> Play</button>
+//     <button class="action-button b2"><svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg" class="Hawkins-Icon Hawkins-Icon-Standard"><path fill-rule="evenodd" clip-rule="evenodd" d="M12 3C7.02944 3 3 7.02944 3 12C3 16.9706 7.02944 21 12 21C16.9706 21 21 16.9706 21 12C21 7.02944 16.9706 3 12 3ZM1 12C1 5.92487 5.92487 1 12 1C18.0751 1 23 5.92487 23 12C23 18.0751 18.0751 23 12 23C5.92487 23 1 18.0751 1 12ZM13 10V18H11V10H13ZM12 8.5C12.8284 8.5 13.5 7.82843 13.5 7C13.5 6.17157 12.8284 5.5 12 5.5C11.1716 5.5 10.5 6.17157 10.5 7C10.5 7.82843 11.1716 8.5 12 8.5Z" fill="currentColor"></path></svg> More Info</button>
+//     `;
+// }
+
+
+function fetchTrendingMovies(){
+    fetchAndbuildMovieSection(apiPaths.fetchTrending, 'Trending Now')
+    .then(list => {
+        const randomIndex = parseInt(Math.random() * list.length);
+        buildBannerSection(list[randomIndex]);
+    }).catch(err=>{
+        console.error(err);
+    });
+}
+
+function buildBannerSection(movie){
+    const bannerCont = document.getElementById('banner-section');
+    
+    bannerCont.style.backgroundImage = `url('${imgPath}${movie.backdrop_path}')`;
+
+    const div = document.createElement('div');
+
+    div.innerHTML = `
+            <h2 class="banner__title">${movie.title}</h2>
+            <p class="banner__info">Trending in movies | Released - ${movie.release_date} </p>
+            <p class="banner__overview">${movie.overview && movie.overview.length > 200 ? movie.overview.slice(0,200).trim()+ '...':movie.overview}</p>
+            <div class="action-buttons-cont">
+                <button class="action-button"><svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg" class="Hawkins-Icon Hawkins-Icon-Standard"><path d="M4 2.69127C4 1.93067 4.81547 1.44851 5.48192 1.81506L22.4069 11.1238C23.0977 11.5037 23.0977 12.4963 22.4069 12.8762L5.48192 22.1849C4.81546 22.5515 4 22.0693 4 21.3087V2.69127Z" fill="currentColor"></path></svg> &nbsp;&nbsp; Play</button>
+                <button class="action-button"><svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg" class="Hawkins-Icon Hawkins-Icon-Standard"><path fill-rule="evenodd" clip-rule="evenodd" d="M12 3C7.02944 3 3 7.02944 3 12C3 16.9706 7.02944 21 12 21C16.9706 21 21 16.9706 21 12C21 7.02944 16.9706 3 12 3ZM1 12C1 5.92487 5.92487 1 12 1C18.0751 1 23 5.92487 23 12C23 18.0751 18.0751 23 12 23C5.92487 23 1 18.0751 1 12ZM13 10V18H11V10H13ZM12 8.5C12.8284 8.5 13.5 7.82843 13.5 7C13.5 6.17157 12.8284 5.5 12 5.5C11.1716 5.5 10.5 6.17157 10.5 7C10.5 7.82843 11.1716 8.5 12 8.5Z" fill="currentColor"></path></svg> &nbsp;&nbsp; More Info</button>
+            </div>
+        `;
+    div.className = "banner-content container";
+
+    bannerCont.append(div);
+}
+
+
+
+
+
+
+
+
+
+
+function fetchAndBuildAllSections(){
+    fetch(apiPaths.fetchAllCategories)
+    .then(res => res.json())
+    .then(res => {
+        const categories = res.genres;
+        if(Array.isArray(categories) && categories.length){
+            categories.forEach(category => {
+                fetchAndbuildMovieSection(
+                    apiPaths.fetchMoviesList(category.id),
+                    category.name
+                    );
+                });
+        }
+    })
+    .catch(err => console.error(err));
+}
+
+
+
+
+
+function fetchAndbuildMovieSection(fetchUrl,categoryName)
+{
+    console.log(fetchUrl,categoryName);
+    return fetch(fetchUrl)
+    .then(res => res.json())
+    .then(res => {
+        const movies = res.results;
+        if(Array.isArray(movies) && movies.length){
+            BuildMovieSection(movies,categoryName);
+        }
+        return movies;
+    })
+    .catch(err => console.error(err))
+}
+
+function BuildMovieSection(movies,categoryName) {
+    console.log(movies,categoryName);
+
+    const moviesCont = document.getElementById('movies-cont');
+
+    const moviesListHTML = movies.map(item => {
+        return `
+        <img class="movie-item" src="${imgPath}${item.backdrop_path}" alt="item.title">         
+        `
+    });
+
+    const moviesSectionHTML = `
+        <h2 class="movie-section-heading">${categoryName}<span class="explore-nudge" ><pre> Explore All</pre></span></h2>
+        <div class="movies-row">
+            ${moviesListHTML}
+        </div>
+    `
+    console.log(moviesSectionHTML);
+    
+    const div = document.createElement("div");
+    div.className = "movies-section"
+    div.innerHTML = moviesSectionHTML;
+    
+    moviesCont.append(div);
+
+}
+
+
+window.addEventListener('load',function(){
+    init();
+    this.window.addEventListener('scroll',function(){
+        const header = document.getElementById('header');
+        if(this.window.scrollY > 5) header.classList.add('black-bg')
+        else header.classList.remove('black-bg')
+    })
+});
+
+const imgDiv = document.getElementById("imgDiv");
+const card = document.getElementById("card");
+
+imgDiv.addEventListener("click", () => {
+    card.classList.toggle("color")
+})
+
+
+
+
+
+
+
+
+
+
